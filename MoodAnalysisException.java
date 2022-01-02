899f9368160e53b24f4ddf7d16a65c2ee7786cb6
Anushanallap <anusha.n468@gmail.com>
2022-01-02 21:38:18 +0530

    added file

---
+++ MoodAnalysisException.java
@@ -0,0 +1,11 @@
+package Moodanalysisalgorithm;
+
+	public class MoodAnalysisException extends Exception {
+		
+		String message;
+
+		public MoodAnalysisException(String message) {
+			super(message);
+		}
+
+}
